804243cb39c2d3da02aba1f2a0e3f4df26cb1bde
Kristal Kung <kristalkung@gmail.com>
2021-08-02 21:15:12 +0000

    changed successful logout back to history.go(0)

--- static/js/components/logout.jsx
+++ static/js/components/logout.jsx
@@ -27,7 +27,7 @@ function LogOut() {
     .then(response => response.json())
     .then(data => {
       if (data === "log out successful") {
-        history.push('/')
+        history.go('0')
   
       }
       else {
